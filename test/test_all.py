e49c1cf9ea8e019af3eb83f6909ab50ce9dee881
Terra <ichbinterra@gmail.com>
2021-12-05 15:39:09 +0100

    tests

---
+++ test/test_all.py
@@ -0,0 +1,16 @@
+import unittest
+from src import assembler
+from pathlib import Path
+class TestAssembler(unittest.TestCase):
+	def test_0(self):
+		self.assertEqual(assembler.Program("test/test_0.txt").run({100: 5}), {100: 0})
+		
+	def test_error_index(self):
+		with self.assertRaises(IndexError):
+			assembler.Program("test/test_error_index.txt").run()
+	def test_error_target(self):
+		with self.assertRaises(assembler.TargetError):
+			assembler.Program("test/test_error_target.txt").run()
+
+if __name__ == "__main__":
+	unittest.main()
