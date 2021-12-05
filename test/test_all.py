0886de4531816add4e296b75e2978d4a932d516c
Terra <ichbinterra@gmail.com>
2021-12-05 21:20:06 +0100

    tests: move and copy

--- test/test_all.py
+++ test/test_all.py
@@ -6,6 +6,14 @@ class TestAssembler(unittest.TestCase):
     def test_0(self):
         """Test a program that always ends in 0 for a register"""
         self.assertEqual(assembler.Program("test/test_0.txt").run({100: 5}), {100: 0})
+    def test_move(self):
+        """Move a value from one register to another"""
+        prog = assembler.Program("test/test_move.txt")
+        self.assertEqual(prog.run({100: 0, 101: 5}), {100: 5, 101: 0})
+    def test_copy(self):
+        """Copy a value from one register to another"""
+        prog = assembler.Program("test/test_copy.txt")
+        self.assertEqual(prog.run({100: 0, 101: 5, 102: 0}), {100: 5, 101: 5, 102: 0})
 
     def test_error_index(self):
         """Program that tires to access a command counter beyond the last line"""
